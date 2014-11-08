d8807b666f466d2d0fd8d085e2aa065288df8a88
alebarrios <al.barrios88@gmail.com>
2014-11-08 17:03:19 -0300

    id:834 time:2 comment:agrego item al menu con un popup desplegable para las estadisticas

--- mileem-app/src/com/mileem/MainActivity.java
+++ mileem-app/src/com/mileem/MainActivity.java
@@ -1,17 +1,22 @@
 package com.mileem;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
+import com.mileem.fragments.AmbXBarrioFragment;
 import com.mileem.fragments.NavigationDrawerFragment;
 import com.mileem.fragments.NewSearchFragment;
 import com.pixate.freestyle.PixateFreestyle;
 
 public class MainActivity extends FragmentActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, PopupMenu.OnMenuItemClickListener {
 
     private NavigationDrawerFragment mNavigationDrawerFragment;
     private CharSequence mTitle;
@@ -24,7 +29,10 @@ public class MainActivity extends FragmentActivity
         setContentView(R.layout.activity_main);
         
         PixateFreestyle.init(this);
-
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        
 
         if (savedInstanceState == null){
         	ImageLoader.init(this);
@@ -56,11 +64,11 @@ public class MainActivity extends FragmentActivity
 //            // Only show items in the action bar relevant to this screen
 //            // if the drawer is not showing. Otherwise, let the drawer
 //            // decide what to show in the action bar.
-//            getMenuInflater().inflate(R.menu.main, menu);
+            getMenuInflater().inflate(R.menu.main, menu);
 //            restoreActionBar();
-//            return true;
+            return true;
 //        }
-        return super.onCreateOptionsMenu(menu);
+//        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -69,10 +77,45 @@ public class MainActivity extends FragmentActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+        	getSupportFragmentManager().popBackStack();
+        }
+        if (id == R.id.statistics) {
+        	View anchor = findViewById(R.id.statistics);
+        	showPopup(anchor);
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
+    
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.statistics_popup, popup.getMenu());
+        popup.show();
+    }
+
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		FragmentManager fragmentManager = getSupportFragmentManager(); 
+		switch (item.getItemId()) {
+	        case R.id.ambientes_barrios:
+	        	fragmentManager.beginTransaction()
+	        	.replace(R.id.container,new AmbXBarrioFragment())
+	        	.addToBackStack("busqueda")
+	        	.commit();
+	            return true;
+	        case R.id.precio_metro:
+	           
+	            return true;
+	        case R.id.zonas_cercanas:
+		           
+	            return true;    
+	        default:
+	            return false;
+	    }
+
+	}
 
 }
---
+++ mileem-app/src/com/mileem/fragments/AmbXBarrioFragment.java
@@ -0,0 +1,23 @@
+package com.mileem.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mileem.R;
+
+public class AmbXBarrioFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.fragment_area, container, false);
+		
+		return rootView;
+		
+	}
+
+}
