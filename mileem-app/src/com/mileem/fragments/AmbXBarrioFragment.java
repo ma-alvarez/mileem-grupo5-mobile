3a864418f591fa880155f9bb761ee44e8ef48e9a
alebarrios <al.barrios88@gmail.com>
2014-11-09 15:38:34 -0300

    id:834 time:1 comment:agrego webview basico con un grafico de torta basico

--- mileem-app/src/com/mileem/fragments/AmbXBarrioFragment.java
+++ mileem-app/src/com/mileem/fragments/AmbXBarrioFragment.java
@@ -5,19 +5,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 
 import com.mileem.R;
 
 public class AmbXBarrioFragment extends Fragment {
 
+	private WebView webView;
+	private int num1=1, num2=2, num3=3;
+	 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
-		View rootView = inflater.inflate(R.layout.fragment_area, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_ambxbarrio, container, false);
+		
+        webView = (WebView)rootView.findViewById(R.id.web);
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        
+        webView.getSettings().setJavaScriptEnabled(true); 
+        webView.loadUrl("file:///android_asset/chart.html");
 		
 		return rootView;
 		
+	}
+	
+
+	public class WebAppInterface {
+
+		@JavascriptInterface
+		public int getNum1() {
+			return num1;
+		}
+
+		@JavascriptInterface
+		public int getNum2() {
+			return num2;
+		}
+
+		@JavascriptInterface
+		public int getNum3() {
+			return num3;
+		}
 	}
 
 }
