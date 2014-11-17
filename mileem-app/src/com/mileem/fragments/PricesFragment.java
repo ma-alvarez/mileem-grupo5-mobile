570c87b8cec8a0ebc5e7ddd8e60e8bd3466422f8
airrera <a.irrera@gmail.com>
2014-11-16 21:33:24 -0300

    Se modifica el rango cuando se selecciona USD y ARS

--- mileem-app/src/com/mileem/fragments/PricesFragment.java
+++ mileem-app/src/com/mileem/fragments/PricesFragment.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -30,8 +32,11 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 
 	
 	private RangeSeekBar<Long> seekBarPrice;
+	private RangeSeekBar<Long> seekBarPricebyQuotation;
 	private String quotation;
 	private BootstrapButton bb_ars, bb_usd, bb_allprices, bb_price_from, bb_price_to, bb_quotation;
+	private View rootView;
+	private ViewGroup seekBarPrice_layout;
 	public PricesFragment() {
 		super();
 		new QuotationTask(this).execute();
@@ -41,7 +46,9 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
-		View rootView = inflater.inflate(R.layout.fragment_prices, container, false);
+		
+		Log.e("PriceFragmet", "onCreateView");
+		rootView = inflater.inflate(R.layout.fragment_prices, container, false);
 		
 		bb_ars = (BootstrapButton) rootView.findViewById(R.id.bb_ars);
 		bb_usd = (BootstrapButton) rootView.findViewById(R.id.bb_usd);
@@ -58,10 +65,13 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 		}	
 		
 		bb_quotation.setText(message);		
-			
+		//Set range for ARS
 		setUpRangeSeekBarPrice();
-		ViewGroup seekBarPrice_layout = (ViewGroup) rootView.findViewById(R.id.RSeekBarPrices);
+		seekBarPrice_layout = (ViewGroup) rootView.findViewById(R.id.RSeekBarPrices);
 		seekBarPrice_layout.addView(seekBarPrice);
+		
+		//Setup range for USD
+		//setUpRangeSeekBarPriceForQuotation();
 		
 		setButtonsListeners();
 		
@@ -70,8 +80,8 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 	}
 	
 	private void setButtonsListeners(){
-		bb_ars.setOnTouchListener(new myOnTouchListener());
-		bb_usd.setOnTouchListener(new myOnTouchListener());
+		bb_ars.setOnTouchListener(new myOnTouchListenerForArs());
+		bb_usd.setOnTouchListener(new myOnTouchListenerForUsd());
 		bb_ars.setPressed(true);
 		
 		bb_allprices.setPressed(true);
@@ -79,12 +89,19 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				setDefault();
+				if (bb_ars.isPressed()){
+					setDefault();
+				}
+				else{
+					setDefaultUSD();
+				}
+				
 				return true;
 			}
 		});
 	}
 	
+	//For ARS
 	private void setUpRangeSeekBarPrice(){
 
 		long MIN_PRICE = 0;
@@ -109,7 +126,33 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 		});
 	}
 	
-	private class myOnTouchListener implements OnTouchListener{
+	private void setUpRangeSeekBarPriceForQuotation(){
+
+		long MIN_PRICE = 0;
+		long MAX_PRICE = 10000000;
+		final DecimalFormat df = new DecimalFormat( "#,###,###,##0" );
+		
+		long calculatedMax = MAX_PRICE / Long.parseLong(quotation);
+		
+		seekBarPricebyQuotation = new RangeSeekBar<Long>(MIN_PRICE, calculatedMax, this.getActivity());
+		seekBarPricebyQuotation.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Long>() {
+		        @Override
+		        public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Long minValue, Long maxValue) {
+		                bb_price_from.setText(df.format(minValue));
+		                bb_price_to.setText(df.format(maxValue));
+		           
+		                bb_allprices.setPressed(false);
+		                if((minValue.longValue() == bar.getAbsoluteMinValue().longValue()) && (maxValue.longValue() == bar.getAbsoluteMaxValue().longValue())){
+			                bb_price_from.setText("min");
+			                bb_price_to.setText("max");
+		                	bb_allprices.setPressed(true);
+		                }
+		        }
+		});
+		
+	}
+	
+	private class myOnTouchListenerForArs implements OnTouchListener{
 		
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
@@ -117,9 +160,45 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 			bb_usd.setPressed(false);
 			
 			v.setPressed(true);
-			Log.e("PriceFragmet", "touched");
+			//Log.e("PriceFragmet", "touched");
+			setUpRangeSeekBarPrice();			
+			seekBarPrice_layout = (ViewGroup) rootView.findViewById(R.id.RSeekBarPrices);
+			seekBarPrice_layout.removeAllViews();
+			seekBarPrice_layout.addView(seekBarPrice);
+			seekBarPrice_layout.refreshDrawableState();
 			
-			return true;
+			bb_price_from.setText("min");
+            bb_price_to.setText("max");
+			rootView.refreshDrawableState();
+						
+		    return true;
+
+		}
+	}
+private class myOnTouchListenerForUsd implements OnTouchListener{
+		
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			bb_ars.setPressed(false);
+			bb_usd.setPressed(false);
+			
+			v.setPressed(true);
+			//Log.e("PriceFragmet", "touched");
+
+			
+			setUpRangeSeekBarPriceForQuotation();
+			seekBarPrice_layout = (ViewGroup) rootView.findViewById(R.id.RSeekBarPrices);
+			seekBarPrice_layout.removeAllViews();
+			seekBarPrice_layout.addView(seekBarPricebyQuotation);
+			seekBarPrice_layout.refreshDrawableState();
+			
+			bb_price_from.setText("min");
+            bb_price_to.setText("max");
+			rootView.refreshDrawableState();
+			
+			
+		    return true;
+
 		}
 	}
 	
@@ -142,10 +221,19 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 				sb.append(ConfigManager.CURRENCY);
 				sb.append("usd");
 			}
-			sb.append(ConfigManager.PRICE_FROM);
-			sb.append(seekBarPrice.getSelectedMinValue());
-			sb.append(ConfigManager.PRICE_TO);
-			sb.append(seekBarPrice.getSelectedMaxValue());
+			if (bb_ars.isPressed()){
+				sb.append(ConfigManager.PRICE_FROM);
+				sb.append(seekBarPrice.getSelectedMinValue());
+				sb.append(ConfigManager.PRICE_TO);
+				sb.append(seekBarPrice.getSelectedMaxValue());
+			}
+			else {
+				sb.append(ConfigManager.PRICE_FROM);
+				sb.append(seekBarPricebyQuotation.getSelectedMinValue());
+				sb.append(ConfigManager.PRICE_TO);
+				sb.append(seekBarPricebyQuotation.getSelectedMaxValue());
+			}
+			
 			return sb.toString();
 		}
 	}
@@ -158,6 +246,15 @@ public class PricesFragment extends Fragment implements IPlaceableFragment {
 		bb_price_to.setText("max");
 		bb_allprices.setPressed(true);
 	}
+	
+	public void setDefaultUSD() {
+		seekBarPricebyQuotation.setSelectedMinValue(seekBarPrice.getAbsoluteMinValue());
+		seekBarPricebyQuotation.setSelectedMaxValue(seekBarPrice.getAbsoluteMaxValue());
+		bb_price_from.setText("min");
+		bb_price_to.setText("max");
+		bb_allprices.setPressed(true);
+	}
+	
 	
 	public void setQuotation(String quota){
 		this.quotation = quota;
